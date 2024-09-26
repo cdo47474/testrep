b1fafe8eb0f2523190c289f33f45baf0dae0fb57
Colin <coor9520@colorado.edu>
2024-09-26 14:40:30 -0600

    Added Lab5

--- Homework/HW3.0.py
+++ Homework/HW3.0.py
@@ -3,72 +3,72 @@ import matplotlib.pyplot as plt
 
 # BISECTION:
 
-# def driver():
-#     # use routines
-#     # pow(x,9) - 45*pow(x,8) + 900*pow(x,7) - 10500*pow(x,6) + 78750*pow(x,5) - 393750*pow(x,4) + 1312500*pow(x,3) - 2812500*pow(x,2) + 3515625*x - 1953125
-#     f = lambda x: pow(x,3) + x - 4
-#     a = 1
-#     b = 4
-#     # f = lambda x: np.sin(x)
-#     # a = 0.1
-#     # b = np.pi+0.1
-#     tol = 1e-3
-#     [astar,ier] = bisection(f,a,b,tol)
-#     print('the approximate root is',astar)
-#     print('the error message reads:',ier)
-#     print('f(astar) =', f(astar))
-#     # define routines
-# def bisection(f,a,b,tol):
-# # Inputs:
-# # f,a,b - function and endpoints of initial interval
-# # tol - bisection stops when interval length < tol
-# # Returns:
-# # astar - approximation of root
-# # ier - error message
-# # - ier = 1 => Failed
-# # - ier = 0 == success
-# # first verify there is a root we can find in the interval
-#     fa = f(a)
-#     fb = f(b)
-#     if (fa*fb>0):
-#         ier = 1
-#         astar = a
-#         return [astar, ier]
-#         print("1")
-#     # verify end points are not a root
-#     if (fa == 0):
-#         astar = a
-#         ier =0
-#         return [astar, ier]
-#     if (fb ==0):
-#         astar = b
-#         ier = 0
-#         return [astar, ier]
-#     count = 0
-#     d = 0.5*(a+b)
-#     while (abs(d-a)> tol):
+def driver():
+    # use routines
+    # pow(x,9) - 45*pow(x,8) + 900*pow(x,7) - 10500*pow(x,6) + 78750*pow(x,5) - 393750*pow(x,4) + 1312500*pow(x,3) - 2812500*pow(x,2) + 3515625*x - 1953125
+    f = lambda x: pow(x,3) + x - 4
+    a = 1
+    b = 4
+    # f = lambda x: np.sin(x)
+    # a = 0.1
+    # b = np.pi+0.1
+    tol = 1e-3
+    [astar,ier] = bisection(f,a,b,tol)
+    print('the approximate root is',astar)
+    print('the error message reads:',ier)
+    print('f(astar) =', f(astar))
+    # define routines
+def bisection(f,a,b,tol):
+# Inputs:
+# f,a,b - function and endpoints of initial interval
+# tol - bisection stops when interval length < tol
+# Returns:
+# astar - approximation of root
+# ier - error message
+# - ier = 1 => Failed
+# - ier = 0 == success
+# first verify there is a root we can find in the interval
+    fa = f(a)
+    fb = f(b)
+    if (fa*fb>0):
+        ier = 1
+        astar = a
+        return [astar, ier]
+        print("1")
+    # verify end points are not a root
+    if (fa == 0):
+        astar = a
+        ier =0
+        return [astar, ier]
+    if (fb ==0):
+        astar = b
+        ier = 0
+        return [astar, ier]
+    count = 0
+    d = 0.5*(a+b)
+    while (abs(d-a)> tol):
         
-#         fd = f(d)
+        fd = f(d)
         
-#         if (fd ==0):
+        if (fd ==0):
             
-#             astar = d
-#             ier = 0
-#             return [astar, ier]
-#         if (fa*fd<0):
+            astar = d
+            ier = 0
+            return [astar, ier]
+        if (fa*fd<0):
             
-#             b = d
-#         else:
-#             a = d
-#             fa = fd
-#         d = 0.5*(a+b)
-#         count = count +1
-#     # print('abs(d-a) = ', abs(d-a))
-#     astar = d
-#     ier = 0
-#     print(count)
-#     return [astar, ier]
-# driver()
+            b = d
+        else:
+            a = d
+            fa = fd
+        d = 0.5*(a+b)
+        count = count +1
+    # print('abs(d-a) = ', abs(d-a))
+    astar = d
+    ier = 0
+    print(count)
+    return [astar, ier]
+driver()
 
 
 # FIXED POINT:
@@ -76,45 +76,45 @@ import matplotlib.pyplot as plt
 
 # import libraries
 
-def driver():
-    # test functions
-    # f1 = lambda x: x * (np.pow((1 + (7 - np.pow(x, 5))/(x * x)), 3)) OVERFLOW
-    # f1 = lambda x: x - (np.pow(x, 5) - 7)/(x*x) OVERFLOW
-    # f1 = lambda x: x - (np.pow(x, 5) - 7)/(5*np.pow(x, 4)) WORKS!
-    f1 = lambda x: -np.sin(2*x) + (5*x)/4 - 3/4
-    # fixed point is alpha1 = 1.4987....
-    # f2 = lambda x: 3+2*np.sin(x)
-#fixed point is alpha2 = 3.09...
-    Nmax = 100
-    tol = 1e-10
-# test f1 '''
-    x0 = 5
-    [xstar,ier] = fixedpt(f1,x0,tol,Nmax)
-    print('the approximate fixed point is:',xstar)
-    print('f1(xstar):',f1(xstar))
-    print('Error message reads:',ier)
-#test f2 '''
-    # x0 = 0.0
-    # [xstar,ier] = fixedpt(f2,x0,tol,Nmax)
-    # print('the approximate fixed point is:',xstar)
-    # print('f2(xstar):',f2(xstar))
-    # print('Error message reads:',ier)
-# define routines
-def fixedpt(f,x0,tol,Nmax):
-    ''' x0 = initial guess'''
-    ''' Nmax = max number of iterations'''
-    ''' tol = stopping tolerance'''
-    count = 0
-    while (count <Nmax):
-        count = count +1
-        x1 = f(x0)
-        if (abs(x1-x0) <tol):
-            xstar = x1
-            ier = 0
-            return [xstar,ier]
-        x0 = x1
-    xstar = x1
-    ier = 1
-    print(count)
-    return [xstar, ier]
-driver()
+# def driver():
+#     # test functions
+#     # f1 = lambda x: x * (np.pow((1 + (7 - np.pow(x, 5))/(x * x)), 3)) OVERFLOW
+#     # f1 = lambda x: x - (np.pow(x, 5) - 7)/(x*x) OVERFLOW
+#     # f1 = lambda x: x - (np.pow(x, 5) - 7)/(5*np.pow(x, 4)) WORKS!
+#     f1 = lambda x: -np.sin(2*x) + (5*x)/4 - 3/4
+#     # fixed point is alpha1 = 1.4987....
+#     # f2 = lambda x: 3+2*np.sin(x)
+# #fixed point is alpha2 = 3.09...
+#     Nmax = 100
+#     tol = 1e-10
+# # test f1 '''
+#     x0 = 5
+#     [xstar,ier] = fixedpt(f1,x0,tol,Nmax)
+#     print('the approximate fixed point is:',xstar)
+#     print('f1(xstar):',f1(xstar))
+#     print('Error message reads:',ier)
+# #test f2 '''
+#     # x0 = 0.0
+#     # [xstar,ier] = fixedpt(f2,x0,tol,Nmax)
+#     # print('the approximate fixed point is:',xstar)
+#     # print('f2(xstar):',f2(xstar))
+#     # print('Error message reads:',ier)
+# # define routines
+# def fixedpt(f,x0,tol,Nmax):
+#     ''' x0 = initial guess'''
+#     ''' Nmax = max number of iterations'''
+#     ''' tol = stopping tolerance'''
+#     count = 0
+#     while (count <Nmax):
+#         count = count +1
+#         x1 = f(x0)
+#         if (abs(x1-x0) <tol):
+#             xstar = x1
+#             ier = 0
+#             return [xstar,ier]
+#         x0 = x1
+#     xstar = x1
+#     ier = 1
+#     print(count)
+#     return [xstar, ier]
+# driver()
--- Labs/Lab5/lab5.py
+++ Labs/Lab5/lab5.py
@@ -1,1 +1,106 @@
-# Im gonna code Lab 5 here
+import numpy as np
+from scipy import special
+import matplotlib.pyplot as plt
+
+# MODIFIED BISECTION!!!!
+# NOT REAL BISECTION CODE!!!!
+
+def driver():
+    # use routines
+    # pow(x,9) - 45*pow(x,8) + 900*pow(x,7) - 10500*pow(x,6) + 78750*pow(x,5) - 393750*pow(x,4) + 1312500*pow(x,3) - 2812500*pow(x,2) + 3515625*x - 1953125
+    f = lambda x: np.exp(np.pow(x,2) + 7*x - 30) - 1
+    fprime = lambda x: (2*x + 7)*np.exp(np.pow(x,2) + 7*x - 30)
+    a = 2
+    b = 4.5
+    # f = lambda x: np.sin(x)
+    # a = 0.1
+    # b = np.pi+0.1
+    tol = 1e-10
+    Nmax = 100
+    [astar,ier] = bisection(f,a,b,tol,fprime)
+    print('Midpoint which puts derivative in basin of convergence:',astar)
+    print('the error message from bisection reads:',ier)
+    print("JUST RETURNED ", astar)
+    (p,pstar,info,it) = newton(f,fprime,astar,tol, Nmax)
+    print('the approximate root is ', pstar)
+    print('the error message from newton reads: ', info)
+    print('number of newton iterations:', it)
+    print('f(astar) =', f(pstar))
+    # define routines
+def bisection(f,a,b,tol,fprime):
+# Inputs:
+# f,a,b - function and endpoints of initial interval
+# tol - bisection stops when interval length < tol
+# Returns:
+# astar - approximation of root
+# ier - error message
+# - ier = 1 => Failed
+# - ier = 0 == success
+# first verify there is a root we can find in the interval
+    fa = f(a)
+    fb = f(b)
+    if (fa*fb>0):
+        ier = 1
+        astar = a
+        return [astar, ier]
+        print("1")
+    # verify end points are not a root
+    if (fa == 0):
+        astar = a
+        ier =0
+        return [astar, ier]
+    if (fb ==0):
+        astar = b
+        ier = 0
+        return [astar, ier]
+    count = 0
+    d = 0.5*(a+b)
+    print(fprime(d))
+    while (abs(fprime(d)) >= 1):
+        
+        fd = f(d)
+        
+        if (fd ==0):
+            
+            astar = d
+            ier = 0
+            return [astar, ier]
+        if (fa*fd<0):
+            
+            b = d
+        else:
+            a = d
+            fa = fd
+        d = 0.5*(a+b)
+        count = count +1
+        print(fprime(d))
+    # print('abs(d-a) = ', abs(d-a))
+    print("Final slope:", fprime(d))
+    print("count: ", count)
+    astar = d
+    ier = 0
+    return [f(d), ier]
+
+def newton(f,fp,p0,tol,Nmax):
+
+    p = np.zeros(Nmax+1)
+    print("---------------")
+    print(p0)
+    p[0] = p0
+    for it in range(Nmax):
+        p1 = p0- f(p0)/fp(p0)
+        
+        p[it+1] = p1
+        if (abs(p1-p0) < tol):
+            
+            pstar = p1
+            info = 0
+            return [p,pstar,info,it]
+        p0 = p1
+    print("skip")
+    pstar = p1
+    info = 1
+    
+    return [p,pstar,info,it]
+
+driver()
